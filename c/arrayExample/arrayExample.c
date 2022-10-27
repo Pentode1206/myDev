73e945dce8a58a8360b913ceab1ed03194e61f10
Jim Tiner <james.c.tiner@outlook>
2022-10-27 19:01:38 -0400

    Changes to arrayExample.c

--- c/arrayExample/arrayExample.c
+++ c/arrayExample/arrayExample.c
@@ -1,5 +1,7 @@
 // Array Example - 27OCT22
-// Create an one dimensional int array with 100 elelments., fill the array with integers 0 throuh 99, print the array elements to the terminal
+// Create an one dimensional int array with 100 elelments, fill the array with integers 0 throuh 99.
+// Print the array elements to the terminal in forward and reverse order.
+
 #include<stdio.h>
 int main() {
     int val[100];
@@ -7,6 +9,10 @@ int main() {
         val[j] = j;
     }
     for (int j = 0; j < 100; j++) {
+        printf("%d ",val[j]);
+    }
+    printf("\n\n");
+    for (int j = 99; j > -1; j--) {
         printf("%d ",val[j]);
     }
     printf("\n\n");
