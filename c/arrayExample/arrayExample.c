044bdded7885a91d1dec71e2bfe455044239ada5
Jim Tiner <james.c.tiner@outlook>
2022-10-27 18:50:17 -0400

    Added comments

--- c/arrayExample/arrayExample.c
+++ c/arrayExample/arrayExample.c
@@ -1,3 +1,5 @@
+// Array Example - 27OCT22
+// Create an one dimensional int array with 100 elelments., fill the array with integers 0 throuh 99, print the array elements to the terminal
 #include<stdio.h>
 int main() {
     int val[100];
