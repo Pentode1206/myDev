0fc7e42b04e22df591bd196d1289bfc7af2c490d
Jim Tiner <james.c.tiner@outlook.com>
2022-10-27 20:59:35 -0400

    Changes to arrayExample

--- c/arrayExample/arrayExample.c
+++ c/arrayExample/arrayExample.c
@@ -4,16 +4,18 @@
 
 #include<stdio.h>
 int main() {
+    int tri = 0;
     int val[100];
     for (int j = 0; j < 100; j++) {
-        val[j] = j;
+        tri += j; // Compute jth triangle number in series.
+        val[j] = tri; // Store jth triangle number in val[j].
     }
     for (int j = 0; j < 100; j++) {
-        printf("%d ",val[j]);
+        printf("%d ",val[j]); // Print triangle numbers in acending order.
     }
     printf("\n\n");
     for (int j = 99; j > -1; j--) {
-        printf("%d ",val[j]);
+        printf("%d ",val[j]); // Print triangle numbers in decending order.
     }
     printf("\n\n");
 }
