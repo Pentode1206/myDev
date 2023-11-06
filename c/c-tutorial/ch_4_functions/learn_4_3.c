56930c133a1bc053a15b4ac8cba1a1c66ce6bd1d
Jim Tiner <james.c.tiner@outlook.com>
2023-11-05 20:12:55 -0500

    new code snippet

---
+++ c/c-tutorial/ch_4_functions/learn_4_3.c
@@ -0,0 +1,30 @@
+/* This snippet demonstrates a function 'add_nums' that returns a value
+ * and a function 'prn_sum' that does not return a value.
+ * Based on c-totrial ch. 4.3
+ * 05NOV23 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int num_1 = 150;
+int num_2 = 250;
+int add_nums (int num_1, int num_2); // function prototype
+int prn_sum (int sum_1); // function prototype
+
+int main ()
+{
+	int sum_1 = add_nums (num_1, num_2);
+	prn_sum (sum_1);
+	exit (0);
+}
+
+int add_nums (int num_1, int num_2)
+{
+	return num_1 + num_2 ;
+}
+
+int prn_sum (int sum_1)
+{
+	printf("%d + %d = %d\n", num_1, num_2, sum_1);
+	return 0;
+}
